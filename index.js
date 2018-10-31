b00a5151eeb6ee39d4c272a2c97f675ba62ea04d
Vinicius Azevedo <viniazvd@gmail.com>
2018-10-31 00:45:27 -0300

    observe option config

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const init = callback => new MutationObserver(callback)
 const start = (state, target, config) => state.observe(target, config)
 const stop = state => state.disconnect()
 
+const uuidv1 = require('uuid/v1')
+
 const prototypes = { init, start, stop }
 
 export default {
@@ -9,6 +11,30 @@ export default {
     Object.defineProperty(Vue.prototype, '$observer', {
       get () {
         return Object.assign({}, prototypes)
+      }
+    })
+
+    Vue.mixin({
+      mounted () {
+        const { observe } = this.$options
+        if (!observe) return
+
+        Object
+          .entries(observe)
+          .forEach(([ key, value ]) => {
+            const uniqueKey = uuidv1()
+
+            Vue.set(this.observers, key, uniqueKey)
+
+            this.observers[key] = init(value['callback'])
+            start(this.observers[key], value['target'](), value['config'])
+          })
+      },
+
+      data () {
+        return {
+          observers: {}
+        }
       }
     })
   }
